6fd82db1a0252ee6683766a0d79af9f651b79aff
kwame-Owusu <alfredowusu595@gmail.com>
2025-09-18 20:37:09 +0100

    wip: working on rate limited ui

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
+import RateLimited from "../components/RateLimited";
 
 const HomePage = () => {
+  const [isRateLimited, setRateLimited] = useState(false);
   return (
     <div className="min-h-screen">
       <Navbar />
+      {isRateLimited && <RateLimited />}
     </div>
   );
 };
