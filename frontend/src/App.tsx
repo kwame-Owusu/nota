20eeff70abe5ddb38d4e09d276cc90ef8fc84716
kwame-Owusu <alfredowusu595@gmail.com>
2025-09-18 19:53:13 +0100

    added packages for icons and using axios instead of fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 const App = () => {
   return (
     <>
-      <button className="btn btn-primary flex mb-4 mt-10">daisy</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
