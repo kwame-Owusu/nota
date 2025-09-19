812163d68126ee657cb13a86a7147d73e2ef5a3b
kwame-Owusu <alfredowusu595@gmail.com>
2025-09-19 07:05:23 +0100

    wip: implementing cors, not working as expected, will fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,19 @@ import express from "express"
 import type { Request, Response } from "express"
 import noteRoutes from "./routes/noteRoutes.ts"
 import { connectDB } from "./config/db.ts"
-import rateLimiter from "./middleware/rateLimiter.ts";
+import rateLimiter from "./middleware/rateLimiter.ts"
+import cors from "cors"
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const CLIENT_BASE_URL = process.env.CLIENT_BASE_URL
 
 app.use(express.json());
-
+app.use(cors({
+  origin: CLIENT_BASE_URL
+}));
 app.use(rateLimiter);
+
 app.use("/api/notes", noteRoutes)
 
 app.get("/ping", (_: Request, res: Response) => {
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimited from "../components/RateLimited";
+import axios from "axios";
 
 const HomePage = () => {
-  const [isRateLimited, setRateLimited] = useState(false);
+  const [isRateLimited, setRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/api/notes/");
+        console.log(res.data);
+      } catch (err) {
+        console.log("Error fetching notes,", err);
+      }
+    };
+    fetchNotes();
+  }, []);
   return (
     <div className="min-h-screen">
       <Navbar />
