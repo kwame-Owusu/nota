f03f7370345b980d169ec9f3fb7b2bfb2ac7a111
kwame-Owusu <alfredowusu595@gmail.com>
2025-09-22 20:22:16 +0100

    polished note detail page

--- frontend/src/pages/NoteDetailPage.tsx
+++ frontend/src/pages/NoteDetailPage.tsx
@@ -43,7 +43,23 @@ const NoteDetailPage = () => {
   };
 
   const handleSave = async () => {
-    console.log("saving note");
+    if (!note?.title.trim() || !note.content?.trim()) {
+      toast.error("All fields are required");
+      return;
+    }
+    setSaving(true);
+    try {
+      await api.put(`/notes/${id}`, note);
+      toast.success("Note updated successfully!");
+      navigate("/");
+    } catch (err) {
+      console.log("Error occurred saving note changes", err);
+      if (axios.isAxiosError(err) && err.response?.status !== 200) {
+        toast.error("Failed to saved note changes");
+      }
+    } finally {
+      setSaving(false);
+    }
   };
 
   if (isLoading) {
@@ -57,7 +73,7 @@ const NoteDetailPage = () => {
   console.log(note);
   return (
     <div className="min-h-screen bg-base-200">
-      <div className="container mx-auto px-4 py-8 border">
+      <div className="container mx-auto px-4 py-8">
         <div className="max-w-2xl mx-auto">
           <div className="flex items-center justify-evenly mb-10">
             <Link to={"/"} className="btn btn-ghost">
